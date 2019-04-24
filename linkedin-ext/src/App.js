4dfb0efe377bc4186358dee061aa4b5c531ec0db
Yanrong Xu <xuyanrong@icloud.com>
2019-04-24 19:59:10 -0400

    chrome  extension version NavBar

--- linkedin-ext/src/App.js
+++ linkedin-ext/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
-import './App.css';
+import classes from './App.css'
 
 import Forms from './views/Forms.js';
 
 class App extends Component {
   render() {
-   return (
-     <div className="App">
-      <header>
-        <NavLink to="/forms">Forms</NavLink>
-        &nbsp; | &nbsp;
-        <NavLink to="/dept">Departments</NavLink>
-        &nbsp; | &nbsp;
-        <NavLink to="/scrape">Scrape</NavLink>
-      </header>
-        <Switch>
-          <Route path="/forms" component={Forms} />
-        </Switch>
+    return (
+      <div className={classes.App}>
+        <header className={classes.Nav}>
+          <NavLink activeClassName={classes.active} className={classes.NavLink} to="/forms">Forms</NavLink>
+          
+          <NavLink activeClassName={classes.active} className={classes.NavLink} to="/dept">Departments</NavLink>
+          
+          <NavLink activeClassName={classes.active} className={classes.NavLink} to="/scrape">Scrape</NavLink>
+        </header>
+          <Switch>
+            <Route path="/forms" component={Forms} />
+          </Switch>
 
-     </div>
-   );
+      </div>
+    );
   }
 }
 
