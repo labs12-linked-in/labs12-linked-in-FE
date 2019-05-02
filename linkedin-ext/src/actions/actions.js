8b7037473c58703fb06ed66243d921e1b4d21812
cori-peck <cori.peck@ymail.com>
2019-05-01 20:53:40 -0500

    started get users action, saving to review another branch

--- linkedin-ext/src/actions/actions.js
+++ linkedin-ext/src/actions/actions.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 
+export const GET_USER_START = "GET_USER_START";
+export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
+export const GET_USER_FAILURE = "GET_USER_FAILURE";
+
+
+export const getUser = () => dispatch => {
+    dispatch({ type: GET_USER_START });
+    const userData = JSON.parse(localStorage.getItem(user));
+    const { first_name, last_name, user_id } = userData;
+    axios
+        .post('https://linkedinextension.herokuapp.com/api/auth/login')
+}
