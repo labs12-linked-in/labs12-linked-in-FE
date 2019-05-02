d4b1fa63f8c2f6a68ab274273c2b6c201626dfe7
cori-peck <cori.peck@ymail.com>
2019-05-02 15:21:06 -0500

    pushing to save

--- linkedin-ext/src/App.js
+++ linkedin-ext/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { Route, withRouter, Switch, NavLink } from 'react-router-dom';
+import axios from './axios-instance';
 
 
 import Home from './components/views/Home';
 import Login from './components/views/Login';
+import Forms from './components/views/Forms/Forms';
+import NewForm from './components/views/Forms/NewForm.js';
+import Departments from './components/views/Departments/Departments';
 
 
 
 class App extends Component {
 
-
+componentDidMount() {
+  axios
+    .get('/api/auth/authenticate')
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
   render() {
 
    return (
@@ -21,8 +34,11 @@ class App extends Component {
       </header>
         <Switch>
           <Route  path='/api/auth/login' component={Login} />
-          <Route path="/" render={(props) => <Home {...props} /> } />
-          <Route path="/home" render={(props) => <App {...props} /> } /> 
+          <Route path="/home" render={(props) => <Home {...props} /> } />
+          <Route path="/" render={(props) => <App {...props} /> } /> 
+          <Route path='api/forms' component={Forms} />
+          <Route path='/new-form' component={NewForm} />
+          <Route path='api/departments' component={Departments} />
         </Switch>
      </div>
    );
