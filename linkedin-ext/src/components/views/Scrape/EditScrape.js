3cdb1fac61821aaefd8ef0a71c64316bae5a3b15
cori-peck <cori.peck@ymail.com>
2019-05-13 15:25:41 -0500

    adds pdf export

--- linkedin-ext/src/components/views/Scrape/EditScrape.js
+++ linkedin-ext/src/components/views/Scrape/EditScrape.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import jsPDF from 'jspdf';
 
-class EditScrape extends Component {
+class EditScrape extends React.Component {
+    demoFromHTML() {
 
-    render() {
-        return (
-            <div>
-                <header>
-                <span onClick={() => this.props.history.push("/scrape")}> Cancel </span>
-                </header>
-                <h3>Resume: </h3>
-                <footer>
-                    <button>Export</button>
-                    <button>Submit</button>
-                </footer>
-            </div>
-        )
+    var doc = new jsPDF();
+    doc.text("Resume:", 40, 30)
+    doc.text("Name: John", 60, 50)
+    doc.text("Title: Social Media Manager", 60, 60)
+    doc.text("E-mail: johnsmith@gmail.com", 60, 70)
+    doc.text("Years Experience: 5", 60, 80)
+    doc.save('a4.pdf');
+    }
+
+render() {
+return (
+    <div>
+        <h4>Resume: </h4>
+        <form>
+            <p>Name: </p>
+            <input type="text" name="name" id="name" placeholder="John" />
+            <p>Tilte: </p>
+            <input type="text" name="title" id="title" placeholder="Social Media Manager" />
+            <p>E-mail: </p>
+            <input type="email" name="email" id="email" placeholder="johnsmith@gmail.com" />
+            <p>Years Experience: </p>
+            <input type="text" name="yearsExp" id="yearsExp" placeholder="5" />
+        </form>
+
+        <button onClick={() => this.demoFromHTML()}>PDF FILE</button>
+        <button>Submit</button>
+    </div>
+    );
     }
 }
 
-export default EditScrape
+export default EditScrape;
