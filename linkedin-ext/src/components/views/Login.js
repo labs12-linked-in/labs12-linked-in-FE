9d2154b381ca984bf8aed600f25119e688c64eaf
Brandon L. Borrero <borrero509@outlook.com>
2019-05-14 08:35:37 -0700

    prepare for merge with master

--- linkedin-ext/src/components/views/Login.js
+++ linkedin-ext/src/components/views/Login.js
@@ -37,7 +37,7 @@ class Login extends Component {
         localStorage.setItem("id", response.data.id);
         localStorage.setItem("firstName", response.data.first_name);
         localStorage.setItem("lastName", response.data.last_name);
-        window.OAuth.redirect("linkedin2", `${localApp}/forms`);
+        window.OAuth.redirect("linkedin2", `${deployedApp}/forms`);
       });
     });
   }
