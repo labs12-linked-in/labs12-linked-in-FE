23760fe0558e59ffe268cfca5a11eb9b4e527a40
Brandon L. Borrero <borrero509@outlook.com>
2019-05-15 12:46:10 -0700

    dynamically render fields in edit scrape

--- linkedin-ext/src/components/views/Scrape/EditScrape.js
+++ linkedin-ext/src/components/views/Scrape/EditScrape.js
@@ -1,44 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getField } from "../../../actions/formFieldActions.js";
 
 class EditScrape extends Component {
+  state = {
+    fields: [],
+    scrapedFields: []
+  };
 
-    state = {
-        form: this.props.formToUpdate,
-        fields: []
-    }
+  async componentDidMount() {
+    await this.setState({ fields: this.props.fieldsToUpdate });
+    this.state.fields.map((val, idx) => {
+      this.setState(prevState => ({
+        scrapedFields: [...prevState.scrapedFields, { text: "" }]
+      }));
+    });
+  }
 
-    async componentDidMount() {
-        // await this.props.getField(this.props.formToUpdate.form_id);
-        this.setState({ fields: this.props.fieldsToUpdate });
-    }
+  handleChangeField = e => {
+    let scrapedFields = [...this.state.scrapedFields];
+    scrapedFields[e.target.dataset.id].text = e.target.value;
+    this.setState({ scrapedFields }, () =>
+      console.log(this.state.scrapedFields)
+    );
+  };
 
+  export = e => {};
 
-
-    render() {
-        console.log(this.props.formToUpdate);
-        return (
+  render() {
+    if (this.state.scrapedFields.length != this.state.fields.length) {
+      return <div>Loading</div>;
+    } else {
+      return (
+        <div>
+          <header>
+            <span onClick={() => this.props.history.push("/scrape")}>
+              {" "}
+              Cancel{" "}
+            </span>
+          </header>
+          <h3>Resume: </h3>
+          <form>
+            {this.state.fields.map((val, idx) => {
+              let nameId = `name-${idx}`;
+              return (
+                <div key={idx}>
+                  <label htmlFor={nameId}>{`${
+                    this.state.fields[idx].name
+                  }`}</label>
+                  <div>
+                    <input
+                      type="text"
+                      name={nameId}
+                      data-id={idx}
+                      id={nameId}
+                      value={this.state.scrapedFields[idx].text}
+                      className="name"
+                      onChange={this.handleChangeField}
+                    />
+                  </div>
+                </div>
+              );
+            })}
             <div>
-                <header>
-                <span onClick={() => this.props.history.push("/scrape")}> Cancel </span>
-                </header>
-                <h3>Resume: </h3>
-                <footer>
-                    <button>Export</button>
-                </footer>
+              <button onClick={e => this.export(e)}>Export</button>
             </div>
-        )
+          </form>
+        </div>
+      );
     }
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        // formToUpdate: state.formReducer.formToUpdate.form,
-        fieldsToUpdate: state.formReducer.fieldsToUpdate
-    };
+  return {
+    fieldsToUpdate: state.formReducer.fieldsToUpdate
+  };
 };
 
-
 export default connect(
-    mapStateToProps, {getField})(EditScrape);
+  mapStateToProps,
+  null
+)(EditScrape);
