c2ddb8f173fa64b5f9e34b1bbfa11cea8dbcac0d
mattr21 <matt.rothstein21@gmail.com>
2019-05-15 21:40:10 -0700

    commented out department code from reducers > index.js

--- linkedin-ext/src/reducers/index.js
+++ linkedin-ext/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './UserReducer';
 import { formReducer } from './FormReducer'
-import {deptReducer} from './DeptReducer'
+// import {deptReducer} from './DeptReducer'
 
 export default combineReducers({
     userReducer,
     formReducer,
-    deptReducer
+    // deptReducer
 })
