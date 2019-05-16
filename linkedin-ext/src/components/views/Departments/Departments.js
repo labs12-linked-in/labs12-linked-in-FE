afcd2648095390c217597819c64acb51ec9d92b9
mattr21 <matt.rothstein21@gmail.com>
2019-05-15 21:28:48 -0700

    commented out code in Departments.js

--- linkedin-ext/src/components/views/Departments/Departments.js
+++ linkedin-ext/src/components/views/Departments/Departments.js
@@ -1,53 +1,53 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import { Link } from "react-router-dom";
 
-import Department from "./Department/Department";
-import NavBar from "../NavBar/NavBar";
-import classes from "./Departments.module.css";
-import { getDept } from "../../../actions/deptActions";
+// import Department from "./Department/Department";
+// import NavBar from "../NavBar/NavBar";
+// import classes from "./Departments.module.css";
+// import { getDept } from "../../../actions/deptActions";
 
-class Departments extends Component {
-  componentDidMount() {
-    this.props.getDept();
-  }
+// class Departments extends Component {
+//   componentDidMount() {
+//     this.props.getDept();
+//   }
 
-  render() {
-    let dept = <div>Loading</div>;
-    if (this.props.depts) {
-      dept = (
-        <div className={classes.Departments}>
-          <div>
-            <NavBar />
-          </div>
-          <div>
-            <div className={classes.bold}>Departments</div>
-            <div className={classes.TopSeparation}>
-              <div>Name</div>
-              <div />
-            </div>
-            {this.props.depts.map(dept => (
-              <Department dept={dept} history={this.props.history}  />
-            ))}
-            <Link to={"/new-dept"}>
-              <button>Create New</button>
-            </Link>
-          </div>
-        </div>
-      );
-    }
+//   render() {
+//     let dept = <div>Loading</div>;
+//     if (this.props.depts) {
+//       dept = (
+//         <div className={classes.Departments}>
+//           <div>
+//             <NavBar />
+//           </div>
+//           <div>
+//             <div className={classes.bold}>Departments</div>
+//             <div className={classes.TopSeparation}>
+//               <div>Name</div>
+//               <div />
+//             </div>
+//             {this.props.depts.map(dept => (
+//               <Department dept={dept} history={this.props.history}  />
+//             ))}
+//             <Link to={"/new-dept"}>
+//               <button>Create New</button>
+//             </Link>
+//           </div>
+//         </div>
+//       );
+//     }
 
-    return <div>{dept}</div>;
-  }
-}
+//     return <div>{dept}</div>;
+//   }
+// }
 
-const mapStateToProps = state => {
-  const depts = state.deptReducer.depts;
-  return { depts };
-};
+// const mapStateToProps = state => {
+//   const depts = state.deptReducer.depts;
+//   return { depts };
+// };
 
 
-export default connect(
-  mapStateToProps,
-  { getDept }
-)(Departments);
+// export default connect(
+//   mapStateToProps,
+//   { getDept }
+// )(Departments);
