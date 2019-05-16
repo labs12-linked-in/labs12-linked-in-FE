0beb23506a162c3c2a5531d6bd0718962232e790
mattr21 <matt.rothstein21@gmail.com>
2019-05-16 09:34:46 -0700

    added styled components, started styling navbar and removed old css files

--- linkedin-ext/src/index.js
+++ linkedin-ext/src/index.js
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-import "./index.css";
 import App from "./App";
 import reducer from "./reducers/index";
 
